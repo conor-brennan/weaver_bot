1cd793c0537a6f386fd7deb01660ea9eed0cb13f
conor-brennan <61962194+conor-brennan@users.noreply.github.com>
2022-06-29 17:35:00 -0700

    implementing the search
    attempted to implement a modified BFS, does not work yet. added a "graph" (dictionary) of connected words along with the code to generate the graph
    
    updated var names for consistency

--- weaver_bot.py
+++ weaver_bot.py
@@ -1,24 +1,66 @@
+import math
+from queue import Queue
+import weaver_vars
+
 AllWords = []
 with open('4letterwords_official.txt') as file:
     lines = file.readlines()
 for l in lines:
     AllWords.append(l[:4])
 
-def lets_in_common(stringA, stringB):
-    if len(stringA) != 4 or len(stringB) != 4:
+def lets_in_common(string_a, string_b):
+    if len(string_a) != 4 or len(string_b) != 4:
         return 0
     score = 0
-    lowerA = stringA.lower()
-    lowerB = stringB.lower()
+    lower_a = string_a.lower()
+    lower_b = string_b.lower()
     for i in range(4):
-        if lowerA[i] == lowerB[i]:
+        if lower_a[i] == lower_b[i]:
             score += 1
     return score
 
-def next_words(currentWord):
+def next_words(current_word):
     result = []
     for w in AllWords:
-        if lets_in_common(currentWord, w) == 3:
+        if lets_in_common(current_word, w) == 3:
             result.append(w)
     return result
+
+def generate_graph():
+    word_graph = {}
+    for current_word in AllWords:
+        word_graph[current_word] = next_words(current_word)
+        print("'{}': {},".format(current_word,word_graph[current_word]))
+
+
+def search(root, goal):
+    q = Queue(maxsize = len(AllWords))
+    visited = {}
+    visited[root] = ""
+    q.put(root)
+    while not q.empty():
+        current_word = q.get()
+        if current_word == goal:
+##            return visited
+            print(current_word)
+            return
+        for word in next_words(current_word):
+            if not word in visited:
+                # labels words with their path
+                visited[word] = current_word
+                q.put(word)
+
+def finish(root, goal):
+    visited = search(root, goal)
+    path = [goal]
+    current = visited[goal]
+    while True:
+        if not current == "":
+            path.append(current)
+            current = visited[current]
+        else:
+            break
+    print(path)
+        
+    
 
--- weaver_graph_generator.py
+++
@@ -1,4 +0,0 @@
-import weaver_bot
-
-print(weaver_bot.next_words("trak"))
-
